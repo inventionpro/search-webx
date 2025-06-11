6dc0df8d756897948ed40d43fba485b96f4178fb
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-11 17:55:06 +0200

    Functionality

---
+++ script.lua
@@ -0,0 +1,20 @@
+function search()
+    local res = fetch({
+        url = "https://api.fsh.plus/buss?new=true&q=" .. get('query').get_contents(),
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" },
+        body = ''
+    })
+    local html = ''
+    for i, s in ipairs(res) do
+        html += '<div style="--color:' .. s.color .. '"><img src="' .. s.favicon .. '"><div><a href="buss://' .. s.url .. '">' .. (s.quality and "⭐" or "") .. s.title .. '</a><p>' .. s.desc .. '</p></div></div>'
+    end
+    get('results').set_contents(html)
+end
+
+get('search').on_click(function()
+    search()
+end)
+get('query').on_submit(function()
+    search()
+end)
