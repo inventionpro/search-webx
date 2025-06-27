08ec8ee5c75448d446009bf5df3c93fea0fb8b29
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-06-27 21:42:14 +0200

    New path

--- script.lua
+++ script.lua
@@ -7,12 +7,12 @@ function search()
     })
     local html = ''
     if window ~= nil then
-        for i, s in ipairs(res) do
+        for i, s in ipairs(res.results) do
             html = html .. '<div style="--color:' .. s.color .. '">' .. (#s.favicon>0 and ('<img src="' .. s.favicon .. '" onerror="this.remove()">') or "") ..
                 '<div><a href="buss://' .. s.url .. '">' .. (s.quality==2 and '⭐' or (s.quality==1 and '✔️' or '')) .. (#s.title>0 and s.title or s.url) .. '</a><p>' .. s.desc .. '</p></div></div>'
         end
     else
-        for i, s in ipairs(res) do
+        for i, s in ipairs(res.results) do
             html = html .. (s.quality==2 and '⭐' or (s.quality==1 and '✔️' or '')) .. (#s.title>0 and (s.title .. ' (' .. s.url .. ')') or s.url) .. (#s.desc>0 and ('\n' .. s.desc) or '') .. '\n'
         end
     end
